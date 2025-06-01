b41a7ef6baeb6745f22954cce2000f3464d84232
ydmins <dr.ydmins@gmail.com>
2025-06-01 16:22:31 +0900

    feature: add test code for init_screen

---
+++ test/init_screen_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:p_20250601/screens/init_screen.dart';
+
+void main() {
+  testWidgets('InitScreen renders correct texts.', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: InitScreen()));
+
+    expect(find.text('Init Screen'), findsOneWidget);
+    expect(
+        find.text(
+            'Morgen will ich sagen genau so wie jetzt: Ich übe heute auch wie ich gestern gemacht habe.'),
+        findsOneWidget);
+  });
+}
